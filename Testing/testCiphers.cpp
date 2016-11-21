b9ca5e5aa8631b4c87ec1b367f71c3d33b57799a
James Pillow <j.pillow@warwick.ac.uk>
2016-11-21 15:22:13 +0000

    Added tests using cipherFactory

--- Testing/testCiphers.cpp
+++ Testing/testCiphers.cpp
@@ -3,13 +3,42 @@
 #include "catch.hpp"
 
 #include <iostream>
+#include <memory>
+#include <vector>
 
 #include "CaesarCipher.hpp"
 #include "PlayfairCipher.hpp"
 #include "VigenereCipher.hpp"
 #include "Cipher.hpp"
+#include "CipherFactory.hpp"
 
 
+TEST_CASE( "Test encryption and decryption of ciphers", "[Cipher]") {
+	
+	std::string cc {"10"};
+	std::string pc {"hello"};
+	std::string vc {"key"};
+
+	// Create list of ciphers
+	std::vector<std::unique_ptr<Cipher>> testCiphers = {};
+	testCiphers.push_back( cipherFactory( CipherType::Caesar,cc ) );
+	testCiphers.push_back( cipherFactory( CipherType::Playfair,pc ) );
+	testCiphers.push_back( cipherFactory( CipherType::Vigenere,vc ) );
+	
+	// Create lists of what encryption and decryption knowns
+	std::vector<std::string> encryptVec = { "HELLOWORLD","THEDOGSATONTHEMATS","HELLOWORLD" };
+	std::vector<std::string> decryptVec = { "ROVVYGYBVN","QOLCAFULYFTYELPLUT","RIJVSUYVJN" };
+
+	// Run loop over lists
+	for (size_t i{0} ; i < testCiphers.size() ; i++) {
+		REQUIRE( testCiphers[i]->applyCipher(encryptVec[i],CipherMode::Encrypt) == decryptVec[i] );
+		REQUIRE( testCiphers[i]->applyCipher(decryptVec[i],CipherMode::Decrypt) == encryptVec[i] );
+	}
+}
+
+
+
+/*
 bool testCipher( const Cipher& cipher, const CipherMode mode, const std::string& inputText, const std::string& outputText)
 {
 	return cipher.applyCipher( inputText, mode ) == outputText;
@@ -43,4 +72,4 @@ TEST_CASE("Vigenere Cipher encryption", "[Vigenere]") {
 TEST_CASE("Vigenere Cipher decryption", "[Vigenere]") {
   VigenereCipher cc{"key"};
   REQUIRE( testCipher ( cc, CipherMode::Decrypt, "RIJVSUYVJN", "HELLOWORLD") );
-}
+}*/
