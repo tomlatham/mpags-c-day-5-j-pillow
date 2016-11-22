c5e0bfd17d956e22afb42a7d3af4c3a00b7a28b6
James <jamesepillow@gmail.com>
2016-11-22 14:38:58 +0000

    Improved commenting

--- MPAGSCipher/CipherFactory.cpp
+++ MPAGSCipher/CipherFactory.cpp
@@ -9,22 +9,29 @@
 
 std::unique_ptr<Cipher> cipherFactory ( const CipherType type, const std::string& key ) 
 {
-	switch(type) {
+	switch(type) { // Switch on the type of cipher to be used
 			
 		case CipherType::Caesar :
+			
 			if ( ! key.empty() ) {
-				for ( const auto& elem : key ) {
-	    			if ( ! std::isdigit(elem) ) {
+				
+				for ( const auto& elem : key ) { // For Caesar cipher check if key is numeric
+	    			
+					if ( ! std::isdigit(elem) ) {
 	      				std::cerr << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
 								  << "the supplied key (" << key << ") could not be successfully converted" << std::endl;
-	      				return std::unique_ptr<Cipher>();
+	      				return std::unique_ptr<Cipher>(); // If key is not numeric return null pointer
 	    			}
-	  			}
+	  			
+				}
+				
 				size_t cKey = std::stoul(key);
 				return std::make_unique<CaesarCipher>( cKey );
+			
 			}
+			
 			else {
-				return std::unique_ptr<Cipher>();
+				return std::unique_ptr<Cipher>(); // If key is empty return null pointer
 			}
 				
 		case CipherType::Playfair :
@@ -33,5 +40,6 @@ std::unique_ptr<Cipher> cipherFactory ( const CipherType type, const std::string
 		case CipherType::Vigenere :
 			return std::make_unique<VigenereCipher>( key );
 	}
+	
 	return std::unique_ptr<Cipher>();
 }
