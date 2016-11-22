8feed6290ceddcbe398968d8d1067be721d24560
James <jamesepillow@gmail.com>
2016-11-22 14:26:58 +0000

    Added doxygen comments to VigenereCipher.hpp

--- MPAGSCipher/VigenereCipher.hpp
+++ MPAGSCipher/VigenereCipher.hpp
@@ -10,20 +10,44 @@
 #include "CaesarCipher.hpp"
 #include "Cipher.hpp"
 
+/**
+	* \file VigenereCipher.hpp
+	* \brief Contains the declaration of the VigenereCipher class
+*/
+
+/**
+	* \class VigenereCipher
+	* \brief Encrypt or decrypt text using the Vigenere cipher with the given key
+*/
 class VigenereCipher : public Cipher {
 	public:
+	/**
+	* Create a new VigenereCipher with the given key
+	*
+	* \param key the key to use in the cipher
+	*/
 	VigenereCipher ( const std::string& key );
 	
+	/**
+	* Set the key to be used for the ecnryption/decryption
+	*
+	* \param key the key to use in the cipher
+	*/
 	void setKey ( const std::string& key );
 	
+	/**
+	* Apply the cipher to the provided text
+	* \param inputText the text to encrypt or decrypt
+	* \param cipherMode wehter to encrypt or decrypt inputText
+	* \return the result of applying the cipher to inputText
+	*/
 	virtual std::string applyCipher ( const std::string& inputText, const CipherMode cipherMode ) const override;
 	
 	private:
-	
 	/// The cipher key
 	std::string key_="";
 	
-	/// Lookup table
+	/// Lookup table for the chars
 	std::map<char,CaesarCipher> charLookup_;
 };
 
