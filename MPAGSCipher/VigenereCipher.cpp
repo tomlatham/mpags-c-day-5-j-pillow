ac45b552c6a539cba6ef364913581c72ec99b6dd
James <jamesepillow@gmail.com>
2016-11-22 14:38:48 +0000

    Improved commenting

--- MPAGSCipher/VigenereCipher.cpp
+++ MPAGSCipher/VigenereCipher.cpp
@@ -31,25 +31,25 @@ void VigenereCipher::setKey ( const std::string& key )
 		if ( charLookup_.find(elem) != charLookup_.end() ) { // Check for repeated letters
 			continue;
 		}
-		size_t index = Alphabet::alphabet.find(elem);
-		CaesarCipher cipher { index }; // Create a CaesarCipher using this pos as a key
-		charLookup_.insert( std::make_pair(elem,cipher) ); // Insert a std::pair of the letter and CaesarCipher into the lookup
+		size_t index = Alphabet::alphabet.find(elem); 		// Set an index according to position in the alphabet
+		CaesarCipher cipher { index }; 						// Create a CaesarCipher using this pos as a key
+		charLookup_.insert( std::make_pair(elem,cipher) ); 	// Insert a std::pair of the letter and CaesarCipher into the lookup
 	}
 }
 
 std::string VigenereCipher::applyCipher ( const std::string& inputText, const CipherMode cipherMode ) const
 {
 
-	/* For each letter in input:
-		- Find the corresponding letter in the key,
-		  repeating/truncating as required
-		  */
+	// For each letter in input: Find the corresponding letter in the key, repeating/truncating as required
+	
+	// Create output string
 	std::string outputText{""};
+	
 	for ( size_t i{0} ; i < inputText.length() ; i++ ) {
-		const char keyLet = key_[ i % key_.length() ]; 
-		const auto cipher = (charLookup_.find( keyLet ))->second; // Find what the shift value for that letter is
-		const std::string letterStr{inputText[i]}; 
-		outputText += cipher.applyCipher( letterStr, cipherMode );	// Caesar Cipher (De)Encryption	 // Add to the output text
+		const char keyLet = key_[ i % key_.length() ]; 				// Find corresponding letter of key. Loop over key if needed
+		const auto cipher = (charLookup_.find( keyLet ))->second; 	// Find what the shift value for that letter is
+		const std::string letterStr{inputText[i]}; 					// Convert inputText letter from char to std::string
+		outputText += cipher.applyCipher( letterStr, cipherMode );	// Apply encryption/decryption, and add to output text
 	}
 	return outputText;
 }
